8ef855d2e3464148038f83a7e52ed73544e2460b
CITnDev <citndev@gmail.com>
2016-10-14 13:40:54 +0200

    Fix TimeBandItem position with a new converter

---
+++ src/CnDTimeControls/Converters/TimeBandItemLocationConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing.Printing;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CnDTimeControls.Converters
+{
+    public class TimeBandItemLocationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var d = (double)value/2;
+            return new Thickness(-d,0,0,0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
